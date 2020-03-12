95220e2e72ab45e0a41b8584ce5308b9c8a441d4
saleszera <crowzcoleslaw@gmail.com>
2020-03-12 00:54:14 -0300

    Adicionada a função de refresh na pagina e redirecionamento para a pagina do projeto, caso ele seja clicado

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -23,9 +23,10 @@ export default class User extends Component {
     title: navigation.getParam('user').name,
   });
 
-  static PropTypes = {
+  static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
+      navigate: PropTypes.func,
     }).isRequired,
   };
 
@@ -33,6 +34,7 @@ export default class User extends Component {
     stars: [],
     loading: true,
     page: 1,
+    refreshing: false,
   };
 
   async componentDidMount() {
@@ -52,6 +54,7 @@ export default class User extends Component {
       stars: page >= 2 ? [...stars, ...response.data] : response.data,
       page,
       loading: false,
+      refreshing: false,
     });
   };
 
@@ -63,9 +66,20 @@ export default class User extends Component {
     this.load(nextPage);
   };
 
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load);
+  };
+
+  handleNavigate = repository => {
+    console.tron.log(repository);
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
+
   render() {
     const { navigation } = this.props;
-    const { stars, loading } = this.state;
+    const { stars, loading, refreshing } = this.state;
     const user = navigation.getParam('user');
 
     return (
@@ -81,9 +95,13 @@ export default class User extends Component {
         ) : (
           <Stars
             data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             keyExtractor={star => String(star.id)}
             renderItem={({ item }) => (
-              <Starred>
+              <Starred onPress={() => this.handleNavigate(item)}>
                 <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
                 <Info>
                   <Title>{item.name}</Title>
@@ -91,8 +109,6 @@ export default class User extends Component {
                 </Info>
               </Starred>
             )}
-            onEndReachedThreshold={0.2}
-            onEndReached={this.loadMore}
           />
         )}
       </Container>
